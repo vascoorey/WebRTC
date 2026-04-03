0f8fe4a34f2f652e67cd67537c05f8c2bf218268
Reis De Orey, Vasco Maria <vasco.orey@gmail.com>
2026-04-03 19:44:37 +1100

    146.0.1 — fix macOS headers for Xcode 26

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/vascoorey/WebRTC/releases/download/146.0.0/WebRTC-M146-fixed.xcframework.zip",
+            url: "https://github.com/vascoorey/WebRTC/releases/download/146.0.1/WebRTC-M146-fixed.xcframework.zip",
             checksum: "74d437cf86e7649a0a3f5c9ef116a1f71ed4919eb39b69184aa6bb03e348c950"
         ),
     ]
