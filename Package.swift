dc8cf326d57ac4989515d53c641be2f8609cfaa7
Reis De Orey, Vasco Maria <vasco.orey@gmail.com>
2026-04-03 19:34:58 +1100

    Fix for Xcode 26

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/stasel/WebRTC/releases/download/146.0.0/WebRTC-M146.xcframework.zip",
-            checksum: "f805abcbe577f838ef86b5ecfd03bd2151dc8709360e720f6ed364a1b7a0bfb2"
+            url: "https://github.com/vascoorey/WebRTC/releases/download/146.0.0/WebRTC-M146-fixed.xcframework.zip",
+            checksum: "74d437cf86e7649a0a3f5c9ef116a1f71ed4919eb39b69184aa6bb03e348c950"
         ),
     ]
 )
